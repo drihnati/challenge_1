84cbd7bb94648bba4de15e802c55385e453befe4
Driheli <drinati01@gmail.com>
2023-04-11 10:43:40 +0100

    add index & challenge1

---
+++ challenge1.js
@@ -0,0 +1,24 @@
+const animalFunction = (animal) {
+
+  if (animal === horse) {
+      return "Neigh!"
+  } 
+  else if (animal === "snake" ) {
+     
+    return "Hiss!"
+
+  }
+   else if (animal === "cat") {
+      
+    return "Yowl!"
+
+  } 
+  else if (animal === "bird") {
+      return "Squawk!"
+  }
+   else {
+    return "none"
+   }
+
+   };
+    
